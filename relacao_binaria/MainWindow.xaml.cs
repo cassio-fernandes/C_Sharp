8681dd484739472d645af113107d04eddc7acafd
Cássio Fernandes <cassio.pereira63@gmail.com>
2016-05-16 00:25:57 -0400

    Relacao_binaria
    Trabalho teórico prático completo sobre relações binárias

---
+++ relacao_binaria/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace relacao_binaria
+{
+    public partial class MainWindow : Window
+    {
+        int intervaloMin, intervaloMax, qtdLinhas;
+        ParesBinarios obterPares;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxParesGerados.Items.Clear();
+            listBoxFSimetrico.Items.Clear();
+
+            labelStatus.Foreground = new SolidColorBrush(Colors.Red);
+
+            if (verificarCampos())
+            {
+                obterPares = new ParesBinarios(intervaloMin, intervaloMax, qtdLinhas);
+                obterPares.gerarPares();
+
+                if (obterPares.verificarSimetria())
+                {
+                    labelStatus.Foreground = new SolidColorBrush(Colors.Green);
+                    labelStatus.Content = "Pares simétricos";
+                }
+                else
+                {
+                    labelStatus.Content = "Pares não simétricos";
+                }
+                // preenche a lista de pares gerados
+                foreach (var p in obterPares.pares)
+                {
+                    listBoxParesGerados.Items.Add(p);
+                }
+                //preenche a lista simétrica 
+                foreach (var p in obterPares.paresSimetricos)
+                {
+                    listBoxFSimetrico.Items.Add(p);
+                }
+            }
+        }
+        
+        private bool verificarCampos()
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxMinimo.Text) && !string.IsNullOrWhiteSpace(textBoxMaximo.Text) && !string.IsNullOrWhiteSpace(textBoxQtd.Text))
+            {   // Conversões de tipos 
+                intervaloMin = Convert.ToInt32(textBoxMinimo.Text);
+                intervaloMax = Convert.ToInt32(textBoxMaximo.Text);
+                qtdLinhas = int.Parse(textBoxQtd.Text);
+
+                if(intervaloMin < intervaloMax) 
+                {   // Qtd deve obedecer: < 10^4; <= (n-m)^2; > 0
+                    if (qtdLinhas <= Math.Pow((intervaloMax - intervaloMin) + 1, 2) && qtdLinhas < Math.Pow(10, 4) && qtdLinhas > 0)
+                    {   // m deve ser <= 10^2
+                        if (intervaloMax <= Math.Pow(10, 2))
+                            return true;
+                        else
+                            MessageBox.Show("O intervalo maximo dever ser 100!");
+                    }
+                    else
+                    {
+                        //labelStatus.Content = "O maximo de linhas são " + Math.Pow((intervaloMax - intervaloMin)+1, 2) + " linhas.\nMinimo 1";
+                        MessageBox.Show("O maximo de linhas são " + Math.Pow((intervaloMax - intervaloMin) + 1, 2) + " linhas.\nMinimo 1");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("O intervalo minimo não pode ser maior\nque o intervalo máximo!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Verifique os campos vazios!");
+            }
+
+            return false;
+        }
+    }
+}
---
+++ relacao_binaria/ParesBInarios.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace relacao_binaria
+{
+    public class ParesBinarios
+    {
+        private int intMin, intMax, quant;
+        private int[,] xy;
+        Random num;
+        public List<string> pares = null;
+        public List<string> paresSimetricos = null;
+
+        //Construtor recebe os valores dos intervalos maximos e míniimo e a quantidade de linhas
+        public ParesBinarios(int n, int m, int q)
+        {
+            intMin = n;
+            intMax = m+1; // O +1 é para que o intervalo de numeros aleatórios vá de min à max, em vez de min à max-1;
+            quant = q;
+        }
+
+        public void gerarPares()
+        {
+            num = new Random();
+            xy = new int[1, 2];
+            int q = quant;
+            do
+            {
+                xy[0, 0] = num.Next(intMin, intMax);
+                xy[0, 1] = num.Next(intMin, intMax);
+                string aux = xy[0, 0].ToString() + "-" + xy[0, 1].ToString();
+
+                if (pares != null)
+                {
+                    foreach (var num in pares)
+                    {   
+                        if(pares.Contains(aux))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            pares.Add(aux);
+                            if (!paresSimetricos.Contains(aux))
+                            {
+                                gerarSimetria(xy[0, 1], xy[0, 0]);
+
+                                if (!(xy[0, 0] == xy[0, 1]))
+                                {
+                                    gerarSimetria(xy[0, 0], xy[0, 1]);
+                                }
+                            }
+                            //  Caso os valores de X e Y sejam iguais, significa que já são simétricos. Não precisa inserir
+
+                            q--;
+                            break;
+                        }
+                    }
+                }
+                else 
+                {
+                    pares = new List<string>();
+                    paresSimetricos = new List<string>();
+                    pares.Add(aux);
+                    gerarSimetria(xy[0, 1], xy[0, 0]);
+
+                    //  Caso os valores de X e Y sejam iguais, significa que já são simétricos. Não precisa inserir
+
+                    if (!(xy[0, 0] == xy[0, 1]))
+                    {
+                        gerarSimetria(xy[0, 0], xy[0, 1]);
+                    }
+                    q--;
+                }
+            } while (q > 0);
+            pares.Sort();
+        }
+
+        private void gerarSimetria(int x, int y)
+        {
+            paresSimetricos.Add(x.ToString() + "-" + y.ToString());
+            paresSimetricos.Sort();
+        }
+
+        public bool verificarSimetria()
+        {
+            int i=0;
+            
+            if (!(quant == Math.Pow((intMax-intMin), 2)))
+            {
+                foreach (var elemento in pares)
+                {
+                    if (paresSimetricos.Contains(elemento))
+                    {
+                        i++;
+                    }
+                }
+
+                //paresSimetricos.Sort();
+
+                if (i != paresSimetricos.Count)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
